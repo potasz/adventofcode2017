10c8c071358b0c70aa2ec4bf14d2cafc99fb3d5a
Szabolcs Pota <szabolcs.pota@gmail.com>
2017-12-17 15:15:40 +0100

    Solution for Day 17.

---
+++ src/main/kotlin/adventofcode2017/potasz/Puzzle17.kt
@@ -0,0 +1,30 @@
+package adventofcode2017.potasz
+
+object Puzzle17 {
+
+    fun solve(jump: Int): Int {
+        val buffer = mutableListOf(0)
+        var pos = 0
+        for (i in 1..2017) {
+            pos = (pos + jump) % buffer.size + 1
+            buffer.add(pos, i)
+        }
+        return buffer[pos + 1]
+    }
+
+    fun solve2(jump: Int): Int {
+        var second = 0
+        var pos = 0
+        for (i in 1..50_000_000) {
+            pos = (pos + jump) % i + 1
+            if (pos == 1) second = i
+        }
+        return second
+    }
+
+    @JvmStatic
+    fun main(args: Array<String>) {
+        println(solve(344))
+        println(solve2(344))
+    }
+}
